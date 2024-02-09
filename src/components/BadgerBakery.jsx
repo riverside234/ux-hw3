d37868e5d5ba4431104847cd3a110621ce5eacd8
riverside234 <bobriverside24@gmail.com>
2024-02-08 22:33:40 -0800

    basic done

--- src/components/BadgerBakery.jsx
+++ src/components/BadgerBakery.jsx
@@ -1,42 +1,61 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import BakedGood from "./BakedGood";
 import { Col, Container, Row } from "react-bootstrap";
 
 export default function BadgerBakery() {
+  const [bakedGoods, setBakedGoods] = useState([]);
 
-    const [bakedGoods, setBakedGoods] = useState([]);
+  useEffect(() => {
+    fetch("https://cs571.org/api/f23/hw3/all-baked-goods", {
+      headers: {
+        "X-CS571-ID": CS571.getBadgerId(),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setBakedGoods(data);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch("https://cs571.org/api/f23/hw3/all-baked-goods", {
-            headers: {
-                "X-CS571-ID": CS571.getBadgerId()
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);
-            setBakedGoods(data);
-        })
-    }, [])
+  const [featured, setfeatured] = useState("");
 
-    return <div>
-        <h1>Badger Bakery</h1>
-        <p>Welcome to our small-town bakery located in Madison, WI!</p>
-        <Container>
-            <Row>
-            {
-                bakedGoods.map(bakedGood => {
-                    return <Col key={bakedGood.name} xs={12} md={6} lg={4} xl={3}>
-                        <BakedGood
-                            name={bakedGood.name}
-                            description={bakedGood.description}
-                            price={bakedGood.price}
-                            featured={bakedGood.featured}
-                        />
-                    </Col>
-                })
-            }
-            </Row>
-        </Container>
+  useEffect(() => {
+    fetch("https://cs571.org/api/f23/hw3/featured-baked-good", {
+      headers: {
+        "X-CS571-ID": CS571.getBadgerId(),
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setfeatured(data);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Badger Bakery</h1>
+      <p>Welcome to our small-town bakery located in Madison, WI!</p>
+      <p>
+        Today's featured item is {featured.name} for ${featured.price}!
+      </p>
+      <Container>
+        <Row>
+          {bakedGoods.map((bakedGood) => {
+            return (
+              <Col key={bakedGood.name} xs={12} md={6} lg={4} xl={3}>
+                <BakedGood
+                  name={bakedGood.name}
+                  description={bakedGood.description}
+                  price={bakedGood.price}
+                  featured={bakedGood.featured}
+                />
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
+  );
 }
--- src/components/BakedGood.jsx
+++ src/components/BakedGood.jsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 
 export default function BakedGood(props) {
-    return <div>
-        <h2>{props.name}</h2>
-        <div>
-            <button className="inline">-</button>
-            <p className="inline">0</p>
-            <button className="inline">+</button>
-        </div>
+  const [amount, setAmount] = useState(0);
+
+  return (
+    <div
+      style={{
+        backgroundColor: props.featured === false ? "white" : "lightblue",
+      }}
+    >
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <p>{"$" + props.price}</p>
+      <div>
+        <button
+          className="inline"
+          disabled={amount === 0 ? true : false}
+          onClick={() => setAmount((preAmount) => preAmount - 1)}
+        >
+          -
+        </button>
+        <p className="inline">{amount}</p>
+        <button
+          className="inline"
+          onClick={() => setAmount((preAmount) => preAmount + 1)}
+        >
+          +
+        </button>
+      </div>
     </div>
+  );
 }
